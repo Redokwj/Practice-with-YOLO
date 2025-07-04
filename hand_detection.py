2318ffe14b0d8f0f26235048c80058d857b15bc5
Taras Fedorchuk <koderwj@gmail.com>
2025-07-04 11:34:13 +0300

    Update hand_detection.py
    Added the ability for hand_detection.py to detect hands in the images.

--- hand_detection.py
+++ hand_detection.py
@@ -5,7 +5,11 @@ model = YOLO("yolov8n-pose.pt")
 
 cap = cv2.VideoCapture(0)  
 
-def check_hand_direction(shoulder, wrist):
+Is_camera = False
+
+image = "test_images/down.png"
+
+def check_hand_status(shoulder, wrist):
     if wrist[1] < shoulder[1]:
         return "HAND UP"
     elif abs(wrist[1] - shoulder[1]) < 80:
@@ -13,31 +17,48 @@ def check_hand_direction(shoulder, wrist):
     else:
         return "HAND DOWN"
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        break
-
-    results = model.predict(source=frame, conf=0.3, save=False, show=False, stream=True)
-    for r in results:
+def detect_keypoints(results):
+   for r in results:
         keypoints = r.keypoints.xy  
         if len(keypoints):
             person = keypoints[0]  
 
             left_shoulder = person[5]
             left_wrist = person[9]
-            left_status = check_hand_direction(left_shoulder, left_wrist)
+            left_status = check_hand_status(left_shoulder, left_wrist)
 
             right_shoulder = person[6]
             right_wrist = person[10]
-            right_status = check_hand_direction(right_shoulder, right_wrist)
-
+            right_status = check_hand_status(right_shoulder, right_wrist)
             
-            cv2.putText(frame, f"Left Hand: {left_status}", (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 0), 3)
-            cv2.putText(frame, f"Right Hand: {right_status}", (20, 110), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 3)
+            return left_status, right_status
+        
+        else:
+            return "No hand detected", "No hand detected"
 
-    cv2.imshow("Pose Detection", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+if Is_camera:
+    while True:
+
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        results = model.predict(source=frame, conf=0.3, save=False, show=False, stream=True)
+        left_status, right_status = detect_keypoints(results)    
+
+        cv2.putText(frame, f"Left Hand: {left_status}", (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (255, 0, 0), 3)
+        cv2.putText(frame, f"Right Hand: {right_status}", (20, 110), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 3)
+
+        cv2.imshow("Pose Detection", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+else:       
+    results = model.predict(source=image, conf=0.3, save=False, show=False)
+    left_status, right_status = detect_keypoints(results)    
+
+    print(f"Left Hand: {left_status}")
+    print(f"Right Hand: {right_status}")
+
 
 
